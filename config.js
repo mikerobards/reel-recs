0cf9a780044bda7f5142e2dc771074096ceadcdb
Michael <mikerobards@icloud.com>
2024-10-23 10:40:50 -0400

    npm install

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ import OpenAI from 'openai';
 import { createClient } from "@supabase/supabase-js";
 
 /** OpenAI config */
-if (!process.env.OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
+if (!import.meta.env.VITE_OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true
 });
